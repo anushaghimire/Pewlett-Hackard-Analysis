2370e3b2a7cea93a7476f394405cdc9bc5a6135f
Anusha GhimireSigdel <85364095+anushaghimire@users.noreply.github.com>
2021-08-12 13:47:46 -0700

    update

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,52 @@
+---Deliverable 1
+
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+order by e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+--Create a Unique Titles table using the INTO clause.
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+SELECT * FROM unique_titles;
+--- create a Retiring Titles table
+SELECT COUNT (ut.emp_no),
+ut.title
+INTO Retiring_titles
+FROM unique_titles as ut
+GROUP BY title 
+ORDER BY COUNT(title) DESC;
+SELECT * FROM retiring_titles;
+
+----Deliverable 2
+---The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+SELECT * FROM mentorship_eligibilty;
